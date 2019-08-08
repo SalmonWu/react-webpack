bc7c616c924677f7dd5e9e7f4b79382578408963
Salmon918 <salmon918@gmail.com>
2019-08-08 17:31:17 +0800

    feat: 新增引入jquery範例
    在index.js新增引入jquery範例

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import 'chosen-js/chosen.css'
 import '~/sass/style.sass'
+const $ = window.jQuery = require('jquery')
+require('chosen-js')
+
 
 class Index extends Component {
+    componentDidMount() {
+        $(".chosen-select").chosen({ no_results_text: "Oops, nothing found!" });
+    }
+
     render() {
         console.log('Index')
         return (
             <div className="Index">
                 <h1>Webpack test index</h1>
+                <select data-placeholder="Choose a country..."
+                    className="chosen-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                </select>
             </div>
         )
     }
--- src/pages/index/render.js
+++ src/pages/index/render.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import $ from 'jquery'
 import Index from '../index/index'
+window.$ = $
+window.jQuery = $
+global.$ = $;
 
 ReactDOM.render(<Index />, document.getElementById('root'))
